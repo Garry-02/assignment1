ba090ae3bdb0cbc148fe2a7d1ca98d64f72a1a03
Garry <gurjyotsingh76@gmail.com>
2021-04-12 19:28:33 +0530

    adding the new and delete buttton

--- JS/app.js
+++ JS/app.js
@@ -1,29 +1,48 @@
-const themebtn = document.querySelector(".themebutton")
-const head = document.querySelector("header")
-const sidebar = document.querySelector(".sidebar")
-const middle = document.querySelector(".middle")
-const footer = document.querySelector("footer")
-const ul = document.querySelector(".headerlist")
-const textarea = document.querySelector("textarea")
+const themebtn = document.querySelector(".themebutton");
+const head = document.querySelector("header");
+const sidebar = document.querySelector(".sidebar");
+const middle = document.querySelector(".middle");
+const footer = document.querySelector("footer");
+const ul = document.querySelector(".headerlist");
+const textarea = document.querySelector("textarea");
 
 
 function themechange() {
 
-    head.classList.toggle("light")
-    head.classList.toggle("dark")
-    sidebar.classList.toggle("sdark")
-    sidebar.classList.toggle("slight")
-    middle.classList.toggle("mdark")
-    middle.classList.toggle("mlight")
-    footer.classList.toggle("dark")
-    footer.classList.toggle("light")
-    ul.classList.toggle("light")
-    ul.classList.toggle("dark")
-    textarea.classList.toggle("tlight")
-    textarea.classList.toggle("tdark")
+    head.classList.toggle("light");
+    head.classList.toggle("dark");
+    sidebar.classList.toggle("sdark");
+    sidebar.classList.toggle("slight");
+    middle.classList.toggle("mdark");
+    middle.classList.toggle("mlight");
+    footer.classList.toggle("dark");
+    footer.classList.toggle("light");
+    ul.classList.toggle("light");
+    ul.classList.toggle("dark");
+    textarea.classList.toggle("tlight");
+    textarea.classList.toggle("tdark");
 
 }
 
+
+var newbtn = document.querySelector(".newbutton");
+const savebtn = document.querySelector(".save");
+const delbtn = document.querySelector(".delete");
+
+function newnote() {
+    newbtn.style.visibility = 'hidden';
+    savebtn.style.visibility = 'visible';
+    delbtn.style.visibility = 'visible';
+    textarea.style.visibility = 'visible';
+}
+
+function del() {
+    newbtn.style.visibility = 'visible';
+    savebtn.style.visibility = 'hidden';
+    delbtn.style.visibility = 'hidden';
+    textarea.style.visibility = 'hidden';
+    textarea.innerHTML = "add your note here";
+}
 
 
 /* new note button 
